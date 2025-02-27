eb83264edbae7f7b005bcee0e5505dae23866ff5
Md Kadir <mdarman18999@gmail.com>
2025-02-27 16:11:48 +0600

    amar sorbanga

---
+++ script/game.js
@@ -0,0 +1,21 @@
+function play(){
+    // hide the Registration
+    const RegistrationDiv = document.getElementById('Registration');
+    RegistrationDiv.style.display = 'none'
+    // console.log(RegistrationDiv.classList)
+    // show the login
+
+    const loginSection = document.getElementById('login');
+    loginSection.style.display = 'block'
+    // console.log(loginSection.classList)
+
+    //  
+}
+
+
+
+
+function reloadPage() {
+    location.reload();
+}
+
